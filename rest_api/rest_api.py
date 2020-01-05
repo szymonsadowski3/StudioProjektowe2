be3fba89bcd531c93aeac5aefad6861704bc1e4c
Chris <kszczyrbak8@gmail.com>
2020-01-05 18:11:32 +0100

    api updated

--- rest_api/rest_api.py
+++ rest_api/rest_api.py
@@ -1,10 +1,13 @@
 from flask import Flask, jsonify, request
 from playhouse.shortcuts import model_to_dict
+from flask_cors import CORS
+
 
 from config import DEFAULTS
-from db.models import *
+from models import *
 
 app = Flask(__name__)
+CORS(app)
 
 
 def get_int_arg(key):
@@ -23,43 +26,63 @@ def handler_by_query(query_object):
     }), 204 if is_response_empty else 200
 
 
-@app.route('/section')
+def count(query_object):
+    try:
+        sections = [model_to_dict(x) for x in query_object]
+    except Exception as e:
+        print(e)
+        sections = []
+    is_response_empty = len(sections) == 0
+    return str(len(sections)), (204 if is_response_empty else 200)
+
+
+@app.route('/sections')
 def get_sections():
     return handler_by_query(Section.select().paginate(get_int_arg('page'), get_int_arg('per_page')))
 
 
-@app.route('/section/<int:section_id>')
+@app.route('/sections/<int:section_id>')
 def get_section_by_id(section_id):
     return handler_by_query(
-        Section.select().where(Section.id == section_id).paginate(get_int_arg('page'), get_int_arg('per_page'))
+        Section.select().where(Section.id == section_id).paginate(
+            get_int_arg('page'), get_int_arg('per_page'))
     )
 
 
-@app.route('/user')
+@app.route('/sections/<int:section_id>/thread')
+def get_threads_by_section_id(section_id):
+    return handler_by_query(
+        Thread.select().join(Section).where(Section.id == section_id)
+    )
+
+
+@app.route('/users')
 def get_users():
     return handler_by_query(User.select().paginate(get_int_arg('page'), get_int_arg('per_page')))
 
 
-@app.route('/user/<int:user_id>')
+@app.route('/users/<int:user_id>')
 def get_user_by_id(user_id):
     return handler_by_query(
-        Section.select().where(User.id == user_id).paginate(get_int_arg('page'), get_int_arg('per_page'))
+        Section.select().where(User.id == user_id).paginate(
+            get_int_arg('page'), get_int_arg('per_page'))
     )
 
 
-@app.route('/thread')
+@app.route('/threads')
 def get_threads():
     return handler_by_query(Thread.select().paginate(get_int_arg('page'), get_int_arg('per_page')))
 
 
-@app.route('/thread/<int:thread_id>')
+@app.route('/threads/<int:thread_id>')
 def get_thread_by_id(thread_id):
     return handler_by_query(
-        Thread.select().where(Thread.id == thread_id).paginate(get_int_arg('page'), get_int_arg('per_page'))
+        Thread.select().where(Thread.id == thread_id).paginate(
+            get_int_arg('page'), get_int_arg('per_page'))
     )
 
 
-@app.route('/thread/<int:thread_id>/followers')
+@app.route('/threads/<int:thread_id>/followers')
 def get_thread_followers_by_id(thread_id):
     return handler_by_query(
         User
@@ -70,27 +93,45 @@ def get_thread_followers_by_id(thread_id):
     )
 
 
-@app.route('/post')
+@app.route('/posts')
 def get_posts():
     return handler_by_query(Post.select().paginate(get_int_arg('page'), get_int_arg('per_page')))
 
 
-@app.route('/post/<int:post_id>')
+@app.route('/posts/<int:post_id>')
 def get_post_by_id(post_id):
     return handler_by_query(
-        Post.select().where(Post.id == post_id).paginate(get_int_arg('page'), get_int_arg('per_page'))
+        Post.select().where(Post.id == post_id).paginate(
+            get_int_arg('page'), get_int_arg('per_page'))
     )
 
 
-@app.route('/private_message')
+@app.route('/threads/<int:thread_id>/posts')
+def get_posts_by_thread_id(thread_id):
+    return handler_by_query(
+        Post.select().join(Thread).where(Thread.id == thread_id).paginate(
+            get_int_arg('page'), get_int_arg('per_page')
+        )
+    )
+
+
+@app.route('/threads/<int:thread_id>/postCount')
+def get_post_count(thread_id):
+    return count(
+        Post.select().join(Thread).where(Thread.id == thread_id)
+    )
+
+
+@app.route('/private_messages')
 def get_private_messages():
     return handler_by_query(PrivateMessage.select().paginate(get_int_arg('page'), get_int_arg('per_page')))
 
 
-@app.route('/private_message/<int:private_message_id>')
+@app.route('/private_messages/<int:private_message_id>')
 def get_private_message_by_id(private_message_id):
     return handler_by_query(
-        PrivateMessage.select().where(PrivateMessage.id == private_message_id).paginate(get_int_arg('page'), get_int_arg('per_page'))
+        PrivateMessage.select().where(PrivateMessage.id == private_message_id).paginate(
+            get_int_arg('page'), get_int_arg('per_page'))
     )
 
 
---
+++ sql/drop.sql
@@ -0,0 +1,6 @@
+drop table post cascade;
+drop table privatemessage cascade;
+drop table section cascade;
+drop table thread cascade;
+drop table threadfollowers cascade;
+drop table "user" cascade;
